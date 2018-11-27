25412d76ba36dc5a87203dbd72e278ca5b1a1f85
ZhangYeSong <17602105720@163.com>
2018-11-27 16:12:13 +0800

    webBlocks dynamically create by data

--- src/index.js
+++ src/index.js
@@ -4,16 +4,24 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 class WebSiteEntity {
-    constructor(url, name) {
+    constructor(url, name, type) {
         this.url = url;
         this.name = name;
+        this.type = type;
         this.icon = url + '/favicon.ico';
     }
 }
 
-let websites = [
-    new WebSiteEntity('https://www.baidu.com', '百度'),
-    new WebSiteEntity('https://github.com', 'github')
+let blockTypesData = [
+    {id: 'lan', name: '编程语言'},
+    {id: 'back', name: '后台开发'},
+    {id: 'front', name: '前端开发'},
+    {id: 'mobile', name: '移动开发'}
+];
+
+let websitesData = [
+    new WebSiteEntity('https://www.baidu.com', '百度', 'lan'),
+    new WebSiteEntity('https://github.com', 'github', 'lan')
 ];
 
 class Website extends React.Component {
@@ -28,31 +36,46 @@ class Website extends React.Component {
 }
 
 class WebBlock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            websites: props.websites
-        }
-    }
-
     renderWebList() {
-        return this.state.websites.map((website) => <Website website={website}/>);
+        return this.props.websites.map((website) => <Website website={website}/>);
     }
 
     render() {
         let webList = this.renderWebList();
         return (
-            <div className="WebBlock">{webList}</div>
+            <div className="WebBlock">
+                <div>{this.props.type.name}</div>
+                <div className="WebList">{webList}</div>
+            </div>
+
         )
     }
 }
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            blockTypes: blockTypesData,
+            websites: websitesData
+        }
+    }
+    renderWebBlocks() {
+        return this.state.blockTypes.map((type) =>
+            <WebBlock
+                type={type}
+                websites={this.state.websites.filter((website) =>
+                    website.type === type.id
+                )
+            }/>
+        )
+    }
     render() {
+        let webBlocks = this.renderWebBlocks();
         return (
             <div className="App">
                 <h1>Judy Map</h1>
-                <WebBlock websites={websites}/>
+                <div>{webBlocks}</div>
             </div>
         )
     }
