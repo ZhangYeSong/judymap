324d150fd0796efe67f5e96432f56e6c7e262ae5
ZhangYeSong <17602105720@163.com>
2018-12-09 22:05:07 +0800

    图标和样式

--- src/index.js
+++ src/index.js
@@ -26,74 +26,74 @@ let blockTypesData = [
 
 let websitesData = [
     //编程语言
-    new WebSiteEntity('http://www.cplusplus.com/', 'C++', 'lan'),
-    new WebSiteEntity('https://www.java.com/', 'Java', 'lan'),
-    new WebSiteEntity('https://www.python.org', 'Python', 'lan', 'http://img2.imgtn.bdimg.com/it/u=1883701644,1651591262&fm=26&gp=0.jpg'),
-    new WebSiteEntity('http://www.php.net', 'PHP', 'lan'),
-    new WebSiteEntity('https://docs.microsoft.com/zh-cn/dotnet/csharp/', 'C#', 'lan', 'http://img2.imgtn.bdimg.com/it/u=98901005,3154284573&fm=26&gp=0.jpg'),
-    new WebSiteEntity('https://golang.org/', 'Go', 'lan'),
-    new WebSiteEntity('http://www.ruby-lang.org/', 'Ruby', 'lan'),
-    new WebSiteEntity('https://swift.org/', 'Swift', 'lan'),
-    new WebSiteEntity('https://www.kotlincn.net/', 'Kotlin', 'lan'),
-    new WebSiteEntity('https://www.w3schools.com/html/ ', 'HTML', 'lan'),
-    new WebSiteEntity('https://www.w3schools.com/css/ ', 'CSS', 'lan'),
-    new WebSiteEntity('https://www.javascript.com/ ', 'Javascript', 'lan'),
-    new WebSiteEntity('http://www.typescriptlang.org/', 'TypeScript', 'lan'),
-    new WebSiteEntity('https://www.scala-lang.org/', 'Scala', 'lan'),
-    new WebSiteEntity('https://www.rust-lang.org/', 'Rust', 'lan'),
-    new WebSiteEntity('https://www.dartlang.org/', 'Dart', 'lan'),
-    new WebSiteEntity('https://www.w3schools.com/sql/', 'SQL', 'lan'),
-    new WebSiteEntity('https://www.haskell.org/', 'Haskell', 'lan', 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1543915034&di=247ed3b3a860a54a87df80d70ef2df78&imgtype=jpg&er=1&src=http%3A%2F%2Fp7.qhimg.com%2Ft010b055b595c67fca9.png'),
+    new WebSiteEntity('http://www.cplusplus.com/', 'C++', 'lan', '/img/c++.jpeg'),
+    new WebSiteEntity('https://www.java.com/', 'Java', 'lan', '/img/java.png'),
+    new WebSiteEntity('https://www.python.org', 'Python', 'lan', '/img/python.png'),
+    new WebSiteEntity('http://www.php.net', 'PHP', 'lan', '/img/php.png'),
+    new WebSiteEntity('https://docs.microsoft.com/zh-cn/dotnet/csharp/', 'C#', 'lan', '/img/csharp.png'),
+    new WebSiteEntity('https://golang.org/', 'Go', 'lan', '/img/go.jpeg'),
+    new WebSiteEntity('http://www.ruby-lang.org/', 'Ruby', 'lan', '/img/ruby.jpeg'),
+    new WebSiteEntity('https://swift.org/', 'Swift', 'lan', '/img/swift.jpeg'),
+    new WebSiteEntity('https://www.kotlincn.net/', 'Kotlin', 'lan', '/img/kotlin.jpeg'),
+    new WebSiteEntity('https://www.w3schools.com/html/ ', 'HTML', 'lan', '/img/html.png'),
+    new WebSiteEntity('https://www.w3schools.com/css/ ', 'CSS', 'lan', '/img/css.png'),
+    new WebSiteEntity('https://www.javascript.com/ ', 'Javascript', 'lan', '/img/javascript.png'),
+    new WebSiteEntity('http://www.typescriptlang.org/', 'TypeScript', 'lan', '/img/typescript.png'),
+    new WebSiteEntity('https://www.scala-lang.org/', 'Scala', 'lan', '/img/scala.png'),
+    new WebSiteEntity('https://www.rust-lang.org/', 'Rust', 'lan', '/img/rust.png'),
+    new WebSiteEntity('https://www.dartlang.org/', 'Dart', 'lan', '/img/dart.png'),
+    new WebSiteEntity('https://www.w3schools.com/sql/', 'SQL', 'lan', '/img/sql.png'),
+    new WebSiteEntity('https://www.haskell.org/', 'Haskell', 'lan', '/img/haskell.png'),
 
     //后台开发
-    new WebSiteEntity('http://spring.io/', 'Spring', 'back'),
-    new WebSiteEntity('http://www.thinkphp.cn/', 'ThinkPHP', 'back'),
-    new WebSiteEntity('https://iris-go.com/', 'Iris', 'back'),
-    new WebSiteEntity('https://nodejs.org/zh-cn/', 'Node.js', 'back'),
-    new WebSiteEntity('https://www.express.com/', 'Express', 'back'),
-    new WebSiteEntity('https://koajs.com/', 'Koa', 'back'),
-    new WebSiteEntity('https://www.djangoproject.com/', 'Django', 'back'),
-    new WebSiteEntity('http://flask.pocoo.org/', 'Flask', 'back'),
-    new WebSiteEntity('https://www.playframework.com/', 'PlayFramework', 'back'),
-    new WebSiteEntity('https://zh-cn.wordpress.com/', 'WordPress', 'back'),
-    new WebSiteEntity('https://hadoop.apache.org/', 'Hadoop', 'back'),
-    new WebSiteEntity('https://spark.apache.org/', 'Spark', 'back'),
-    new WebSiteEntity('https://akka.io/', 'Akka', 'back'),
+    new WebSiteEntity('http://spring.io/', 'Spring', 'back', '/img/spring.jpeg'),
+    new WebSiteEntity('http://www.thinkphp.cn/', 'ThinkPHP', 'back', '/img/thinkphp.png'),
+    new WebSiteEntity('https://iris-go.com/', 'Iris', 'back', '/img/iris.png'),
+    new WebSiteEntity('https://nodejs.org/zh-cn/', 'Node.js', 'back', '/img/nodejs.png'),
+    new WebSiteEntity('https://expressjs.com/', 'Express', 'back', '/img/express.png'),
+    new WebSiteEntity('https://koajs.com/', 'Koa', 'back', '/img/koa.png'),
+    new WebSiteEntity('https://www.djangoproject.com/', 'Django', 'back', '/img/django.jpeg'),
+    new WebSiteEntity('http://flask.pocoo.org/', 'Flask', 'back', '/img/flask.png'),
+    new WebSiteEntity('https://www.playframework.com/', 'PlayFramework', 'back', '/img/playframework.png'),
+    new WebSiteEntity('https://zh-cn.wordpress.com/', 'WordPress', 'back', '/img/wordpress.png'),
+    new WebSiteEntity('https://hadoop.apache.org/', 'Hadoop', 'back', '/img/hadoop.png'),
+    new WebSiteEntity('https://spark.apache.org/', 'Spark', 'back', '/img/spark.png'),
+    new WebSiteEntity('https://akka.io/', 'Akka', 'back', '/img/akka.png'),
 
     //前端开发
-    new WebSiteEntity('https://reactjs.org/', 'React', 'front'),
-    new WebSiteEntity('https://cn.vuejs.org/index.html', 'Vue', 'front'),
-    new WebSiteEntity('https://angular.io/', 'Angular', 'front'),
-    new WebSiteEntity('https://jquery.com/', 'JQuery', 'front'),
-    new WebSiteEntity('https://getbootstrap.com/', 'Bootstrap', 'front'),
-    new WebSiteEntity('https://webpack.js.org/', 'Webpack', 'front'),
-    new WebSiteEntity('https://sass-lang.com/', 'Sass', 'front'),
-    new WebSiteEntity('https://redux.js.org/', 'Redux', 'front'),
-    new WebSiteEntity('https://cn.rx.js.org/', 'RxJs', 'front'),
-    new WebSiteEntity('https://developers.google.com/web/progressive-web-apps/', 'PWA', 'front'),
-    new WebSiteEntity('https://electronjs.org/', 'Electron.js', 'front'),
+    new WebSiteEntity('https://reactjs.org/', 'React', 'front', '/img/react.png'),
+    new WebSiteEntity('https://cn.vuejs.org/index.html', 'Vue', 'front', '/img/vue.jpeg'),
+    new WebSiteEntity('https://angular.io/', 'Angular', 'front', '/img/angular.png'),
+    new WebSiteEntity('https://jquery.com/', 'JQuery', 'front', '/img/jquery.png'),
+    new WebSiteEntity('https://getbootstrap.com/', 'Bootstrap', 'front', '/img/bootstrap.png'),
+    new WebSiteEntity('https://webpack.js.org/', 'Webpack', 'front', '/img/webpack.jpeg'),
+    new WebSiteEntity('https://sass-lang.com/', 'Sass', 'front', '/img/sass.png'),
+    new WebSiteEntity('https://redux.js.org/', 'Redux', 'front', '/img/redux.png'),
+    new WebSiteEntity('https://cn.rx.js.org/', 'RxJs', 'front', '/img/rxjs.jpeg'),
+    new WebSiteEntity('https://developers.google.com/web/progressive-web-apps/', 'PWA', 'front', '/img/pwa.jpeg'),
+    new WebSiteEntity('https://electronjs.org/', 'Electron.js', 'front', '/img/electron.png'),
 
     //移动开发
-    new WebSiteEntity('https://developer.apple.com/ios/', 'ios', 'mobile'),
-    new WebSiteEntity('https://developer.android.com/', 'Android', 'mobile'),
-    new WebSiteEntity('https://facebook.github.io/react-native/', 'React Native', 'mobile'),
-    new WebSiteEntity('https://ionicframework.com/', 'ionic', 'mobile'),
-    new WebSiteEntity('https://flutter.io/', 'Flutter', 'mobile'),
+    new WebSiteEntity('https://developer.apple.com/ios/', 'ios', 'mobile', '/img/ios.jpeg'),
+    new WebSiteEntity('https://developer.android.com/', 'Android', 'mobile', '/img/android.png'),
+    new WebSiteEntity('https://facebook.github.io/react-native/', 'React Native', 'mobile', '/img/reactnative.png'),
+    new WebSiteEntity('https://ionicframework.com/', 'ionic', 'mobile', '/img/ionic.png'),
+    new WebSiteEntity('https://flutter.io/', 'Flutter', 'mobile', '/img/flutter.png'),
 
     //数据库
-    new WebSiteEntity('https://www.mysql.com/cn/', 'MySQL', 'database'),
-    new WebSiteEntity('https://www.oracle.com/database/', 'Oracle', 'database'),
-    new WebSiteEntity('https://www.mongodb.com/cn', 'MongoDB', 'database'),
-    new WebSiteEntity('http://slick.lightbend.com/', 'Slick', 'database'),
+    new WebSiteEntity('https://www.mysql.com/cn/', 'MySQL', 'database', '/img/mysql.png'),
+    new WebSiteEntity('https://www.oracle.com/database/', 'Oracle', 'database', '/img/oracle.jpeg'),
+    new WebSiteEntity('https://www.mongodb.com/cn', 'MongoDB', 'database', '/img/mongodb.png'),
+    new WebSiteEntity('http://slick.lightbend.com/', 'Slick', 'database', '/img/slick.jpg'),
 
     //设计&&UI组件库
-    new WebSiteEntity('https://ant.design/', 'Ant-Design', 'design'),
-    new WebSiteEntity('https://developer.apple.com/design/', 'Apple-Design', 'design'),
-    new WebSiteEntity('https://material.io/design/', 'Material', 'design'),
-    new WebSiteEntity('https://lanhuapp.com/', '蓝湖', 'design'),
-    new WebSiteEntity('https://www.photoshop.com/', 'PhotoShop', 'design'),
-    new WebSiteEntity('https://www.sketchapp.com/', 'Sketch', 'design'),
-    new WebSiteEntity('http://www.iconfont.cn/', '阿里矢量图库', 'design'),
+    new WebSiteEntity('https://ant.design/', 'Ant-Design', 'design', '/img/antdesign.jpeg'),
+    new WebSiteEntity('https://developer.apple.com/design/', 'Apple-Design', 'design', '/img/appledesign.jpeg'),
+    new WebSiteEntity('https://material.io/design/', 'Material', 'design', '/img/material.png'),
+    new WebSiteEntity('https://lanhuapp.com/', '蓝湖', 'design', '/img/lanhu.png'),
+    new WebSiteEntity('https://www.photoshop.com/', 'PhotoShop', 'design', '/img/photoshop.png'),
+    new WebSiteEntity('https://www.sketchapp.com/', 'Sketch', 'design', '/img/sketch.jpeg'),
+    new WebSiteEntity('http://www.iconfont.cn/', '阿里矢量图库', 'design', '/img/alisvg.png'),
 ];
 
 class Website extends React.Component {
@@ -116,7 +116,7 @@ class WebBlock extends React.Component {
         let webList = this.renderWebList();
         return (
             <div className="WebBlock">
-                <div>{this.props.type.name}</div>
+                <div className="BlockTitle">{this.props.type.name}</div>
                 <div className="WebList">{webList}</div>
             </div>
 
@@ -147,7 +147,7 @@ class App extends React.Component {
         return (
             <div className="App">
                 <h1>Judy Map</h1>
-                <div>{webBlocks}</div>
+                <div className="WebBlocks">{webBlocks}</div>
             </div>
         )
     }
